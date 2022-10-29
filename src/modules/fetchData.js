2477cc346ed10648736269f573cafd8527870d8c
UmerZia <omerzia73@gmail.com>
2022-10-30 02:44:29 +0500

    Add functionality of fetch data

---
+++ src/modules/fetchData.js
@@ -0,0 +1,24 @@
+import { GameId } from "./newGame.js";
+
+const container = document.getElementById("data-container");
+const getData = async () => {
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${GameId}/scores/`
+  );
+  if (!response.ok) {
+    throw new Error(`Something Went Wrong status: ${response.status}`);
+  }
+  const data = await response.json();
+  if (GameId !== null) {
+    let item = "";
+    data.result.forEach((score) => {
+      // item += displayItem(score);
+      console.log("game id  is null=", GameId);
+      item += `<li><spa>${score.user} </spa><span>${score.score}</span></li>`;
+    });
+    container.innerHTML = item;
+  } else {
+    container.innerHTML = "Please Creat A new Game";
+  }
+};
+export default getData;
